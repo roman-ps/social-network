ac34b015a957a0e48242e85ec0e59450b4b5eb3b
Pozharov Roman <ru.roman.ps@gmail.com>
2021-08-03 22:12:42 +0400

    NavLink

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import React from 'react';
+import {Route, BrowserRouter} from 'react-router-dom';
 
 import Profile from './components/Profile/Profile';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
+import Dialogs from './components/Dialogs/Dialogs';
+import News from './components/News/News';
+import Music from './components/Music/Music';
+import Settings from './components/Settings/Settings';
 
 import classes from './App.module.css';
 
 const App = () => {
   return (
-    <div className={classes.main}>
-      <Header />
-      <Navbar />
-      <Profile />
-    </div>
+    <BrowserRouter>
+      <div className={classes.main}>
+        <Header />
+        <Navbar />
+        <div className={classes.wrapper}>
+          <Route path='/profile' component={Profile} />
+          <Route path='/dialogs' component={Dialogs} />
+          <Route path='/news' component={News} />
+          <Route path='/music' component={Music} />
+          <Route path='/settings' component={Settings} />
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import {NavLink} from 'react-router-dom';
 import classes from './Navbar.module.css';
 
 const Navbar = () => {
   return (
     <nav className={classes.nav}>
       <ul className={classes.menu}>
-        <li className={classes.item}><a className={`${classes.link} ${classes.active}`} href='profile.html'>Profile</a></li>
-        <li className={classes.item}><a className={classes.link} href='messages.html'>Messages</a></li>
-        <li className={classes.item}><a className={classes.link} href='new.html'>News</a></li>
-        <li className={classes.item}><a className={classes.link} href='music.html'>Music</a></li>
-        <li className={classes.item}><a className={classes.link} href='settings.html'>Settings</a></li>
+        <li className={classes.item}><NavLink className={`${classes.link}`} to='/profile' activeClassName={classes.active}>Profile</NavLink></li>
+        <li className={classes.item}><NavLink className={classes.link} to='/dialogs' activeClassName={classes.active}>Messages</NavLink></li>
+        <li className={classes.item}><NavLink className={classes.link} to='/news' activeClassName={classes.active}>News</NavLink></li>
+        <li className={classes.item}><NavLink className={classes.link} to='/music' activeClassName={classes.active}>Music</NavLink></li>
+        <li className={classes.item}><NavLink className={classes.link} to='/settings' activeClassName={classes.active}>Settings</NavLink></li>
       </ul>
     </nav>
   );
